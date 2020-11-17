4e090ff8620226c569ea9f2a435726b7dc13c960
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 19:20:54 -0600

    add comment to creatstore

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -2,4 +2,5 @@
 //you need to use commonejs module syntax to be able to run the project with node
 const { createStore } = require('redux')
 
+//use the createstore method to generate your redex store by invoking it with a reducer
 const store = createStore()
