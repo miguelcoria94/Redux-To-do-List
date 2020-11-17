ff5041e9152e576a04cc417087f0c2cc88ec3dcb
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 19:53:40 -0600

    add notes for reducer

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -2,8 +2,11 @@
 //you need to use commonejs module syntax to be able to run the project with node
 const { createStore } = require('redux')
 
+//the reducer is a function that helps manage the redux store by routing actions based on their type attribute
+//define the taskReducer function and have it take in the redux stores state and an action as parameters.
+const tasksReducer = {}
+
 //each app should only have one redux store where all of an app's state is managed
 //unlike react context where a single react app can utilize multiple context
-
 //use the createstore method to generate your redex store by invoking it with a reducer
 const store = createStore()
