d7f2ca0086f8c07266319f1b5945beeaca279908
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 20:02:17 -0600

    add more notes on task resucer

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -5,8 +5,11 @@ const { createStore } = require('redux')
 //the reducer is a function that helps manage the redux store by routing actions based on their type attribute
 //define the taskReducer function and have it take in the redux stores state and an action as parameters.
 //you'll want the state to default to an empty array
-//since you invoke the createStore method with the taskRseducer, you need to make the tasksReducer first
-const tasksReducer = (state = [], action)
+//since you invoke the createStore method with the taskRseducer, you need to make the tasksReducer first 
+//note this project uses an array to store the tasks instead of an object to make it easier to delete a positional task
+const tasksReducer = (state = [], action) => {
+    //TODO: Set up switch statement to manage actions based on type
+}
 
 
 //each app should only have one redux store where all of an app's state is managed
