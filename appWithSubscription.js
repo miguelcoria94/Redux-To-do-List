14dfcaddab5e014975dc4406530ba5213fe2037e
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-17 05:55:51 -0600

    subscribe store

--- appWithSubscription.js
+++ appWithSubscription.js
@@ -7,3 +7,7 @@ const {
 
 console.log('Default Redux Store (empty task list): ');
 console.log(store.getState());
+
+store.subscribe(() => console.log(store.getState()));
+
+console.log("Task creation actions")
