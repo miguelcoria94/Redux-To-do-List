9a6dec3dc0f2216c9f7da3e5e5447790f561d369
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 19:19:39 -0600

    add comments to require

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -1,4 +1,5 @@
-
+//import the createStore method from Redux
+//you need to use commonejs module syntax to be able to run the project with node
 const { createStore } = require('redux')
 
 const store = createStore()
