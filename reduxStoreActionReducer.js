31cae1a4cbf8a7235b96002e5721b9cf32f04c57
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 19:22:52 -0600

    context vs redux store

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -2,5 +2,8 @@
 //you need to use commonejs module syntax to be able to run the project with node
 const { createStore } = require('redux')
 
+//each app should only have one redux store where all of an app's state is managed
+//unlike react context where a single react app can utilize multiple context
+
 //use the createstore method to generate your redex store by invoking it with a reducer
 const store = createStore()
