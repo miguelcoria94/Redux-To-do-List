52a65a924760fd1c304a20d51ef2a56a1d959875
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-16 20:17:25 -0600

    set up actions

--- reduxStoreActionReducer.js
+++ reduxStoreActionReducer.js
@@ -2,6 +2,10 @@
 //you need to use commonejs module syntax to be able to run the project with node
 const { createStore } = require('redux')
 
+const CREATE_TASK = 'CREATE_TASK';
+const DELETE_TASK = 'DELETE_TASK';
+const RESET_TASK_LIST = 'RESET_TASK_LIST';
+
 //the reducer is a function that helps manage the redux store by routing actions based on their type attribute
 //define the taskReducer function and have it take in the redux stores state and an action as parameters.
 //you'll want the state to default to an empty array
@@ -16,3 +20,22 @@ const tasksReducer = (state = [], action) => {
 //unlike react context where a single react app can utilize multiple context
 //use the createstore method to generate your redex store by invoking it with a reducer
 const store = createStore()
+
+//action creators
+
+const createTask = (taskMessage) => ({
+    type: CREATE_TASK,
+    taskMessage,
+});
+
+const deleteTask = (taskId) => ({
+    type: DELETE_TASK,
+    taskId,
+});
+
+const resetTaskList = () => {
+    return {
+        type: RESET_TASK_LIST,
+        emptyTaskList: [],
+    }
+};
