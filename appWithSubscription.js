aa9bae48bb550f51f2e6b184a068dc96d62b0b98
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-17 06:04:10 -0600

    done with project

--- appWithSubscription.js
+++ appWithSubscription.js
@@ -10,4 +10,17 @@ console.log(store.getState());
 
 store.subscribe(() => console.log(store.getState()));
 
-console.log("Task creation actions")
+console.log("Task creation actions");
+
+store.dispatch(createTask("walk dog"));
+store.dispatch(createTask("feed cat"));
+store.dispatch(createTask("talk to bird"));
+store.dispatch(createTask("watch goldfish"));
+console.log(store.getState())
+
+console.log('Task deletion actions');
+store.dispatch(deleteTask(0));
+store.dispatch(deleteTask(1));
+
+console.log('Task reset action');
+store.dispatch(resetTaskList());
