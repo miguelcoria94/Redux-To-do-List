24a070b24a814e13e40fb7c68319efb29e0273ed
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-17 05:41:15 -0600

    add task

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const {
 
 console.log('Default Redux Store (empty task list):');
 console.log(store.getState());
+
+store.dispatch(createTask('walk dog'));
+store.dispatch(createTask('feed cat'));
+store.dispatch(createTask('talk to bird'));
+store.dispatch(createTask('watch goldfish'));
+
+console.log('Redux Store: ')
+console.log(store.getState())
